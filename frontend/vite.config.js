0efc8b2591f0fb4a4df938a398b95329e0961fec
rashi <rashig2344@gmail.com>
2025-09-26 15:57:00 +0530

    klml

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
+  build: {
+    chunkSizeWarningLimit: 3000, // increase limit in KB (default is 500 KB)
   },
 });
